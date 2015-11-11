5dfd3bc746214694e95caaf88bea056405164c59
iibnusalam0 <iibnusalam0@gmail.com>
2015-11-11 10:58:11 +0700

    tambah field type dan file di question

---
+++ console/migrations/m151111_034846_add_column_type_and_file_to_table_question.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151111_034846_add_column_type_and_file_to_table_question extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('question', 'type_file', 'INTEGER NOT NULL default 1');
+        $this->addColumn('question', 'file', 'STRING NULL');
+    }
+
+    public function down()
+    {
+        echo "m151111_034846_add_column_type_and_file_to_table_question cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
